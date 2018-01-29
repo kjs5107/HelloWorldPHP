fff2ee632955e4d0577e31666ffd742d583aa434
Kyle Scagnelli <kjs5107@rit.edu>
2018-01-29 12:56:51 -0500

    Simple PHP setup for a Hello World project.

---
+++ goodbye_world.php
@@ -0,0 +1,14 @@
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My second PHP page</h1>
+
+<?php
+echo "Goodbye World!";
+?>
+
+</body>
+</html>
---
+++ hello_world.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+<div style="margin-top: 10px;">
+    <h1>Hello, World!</h1>
+    <ul>
+        <li>Why are you reading this ... ? Go watch some YouTube instead ... 
+          <a href="https://www.youtube.com/" target="_blank">If You Say So?</a>
+        </li>
+        <li>
+          Click <a href="http://www.se.rit.edu/~kjs5107/BasicWebDev/goodbye_world.php">here</a>
+          once you've read everything else on this page ...
+        </li>
+        <li>Percy Jackson!</li>
+        <img src="percy_jackson.jpg" alt="" style="height: 300px;" />
+        <li>I hope to broaden my horizons in regards to web development. I'm very familiar with how web
+            development works and how to use Angular and Node, however I'm excited to learn about other
+            technologies as well (such as this one, PHP)!
+        </li>
+    </ul>
+</div>
+
+<?php
+echo "Hello World Again! (In a PHP Script Tag)";
+?>
+
+</body>
+</html>
+
